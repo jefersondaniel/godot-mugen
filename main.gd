0759d7d19be8ad0aa5dbf86c2e552df5cf6332ba
Jeferson Daniel <contato@jefersondaniel.com>
2019-04-01 23:36:48 -0300

    Reading palettes

--- main.gd
+++ main.gd
@@ -3,73 +3,160 @@ extends Node2D
 var specs = {
 	'SffHeader': [
 		{'name': 'signature', 'type': 'string', 'length': 12},
-		{'name': 'verlo3', 'type': 'u_char'},
-		{'name': 'verlo2', 'type': 'u_char'},
-		{'name': 'verlo1', 'type': 'u_char'},
-		{'name': 'verhi', 'type': 'u_char'},
+		{'name': 'verlo3', 'type': 'char'},
+		{'name': 'verlo2', 'type': 'char'},
+		{'name': 'verlo1', 'type': 'char'},
+		{'name': 'verhi', 'type': 'char'},
 		{'name': 'reserved1', 'type': 'bytes', 'length': 4},
 		{'name': 'reserved2', 'type': 'bytes', 'length': 4},  
-		{'name': 'compatverlo3', 'type': 'u_char'},
-		{'name': 'compatverlo2', 'type': 'u_char'},
-		{'name': 'compatverlo1', 'type': 'u_char'},
-		{'name': 'compatverhi', 'type': 'u_char'},
+		{'name': 'compatverlo3', 'type': 'char'},
+		{'name': 'compatverlo2', 'type': 'char'},
+		{'name': 'compatverlo1', 'type': 'char'},
+		{'name': 'compatverhi', 'type': 'char'},
 		{'name': 'reserved1', 'type': 'bytes', 'length': 4},
 		{'name': 'reserved2', 'type': 'bytes', 'length': 4},
-		{'name': 'first_sprnode_offset', 'type': 'u_long'},
-		{'name': 'total_frames', 'type': 'u_long'},
-		{'name': 'first_palnode_offset', 'type': 'u_long'},
-		{'name': 'total_palettes', 'type': 'u_long'},
-		{'name': 'ldata_offset', 'type': 'u_long'},
-		{'name': 'ldata_length', 'type': 'u_long'},
-		{'name': 'tdata_offset', 'type': 'u_long'},
-		{'name': 'tdata_length', 'type': 'u_long'},
+		{'name': 'first_sprnode_offset', 'type': 'long'},
+		{'name': 'total_frames', 'type': 'long'},
+		{'name': 'first_palnode_offset', 'type': 'long'},
+		{'name': 'total_palettes', 'type': 'long'},
+		{'name': 'ldata_offset', 'type': 'long'},
+		{'name': 'ldata_length', 'type': 'long'},
+		{'name': 'tdata_offset', 'type': 'long'},
+		{'name': 'tdata_length', 'type': 'long'},
 		{'name': 'reserved5', 'type': 'bytes', 'length': 4},
 		{'name': 'reserved6', 'type': 'bytes', 'length': 4},
 		{'name': 'unused', 'type': 'bytes', 'length': 436},
 	],
+	'SffPalHeader': [
+		{'name': 'groupno', 'type': 'short'},
+		{'name': 'itemno', 'type': 'short'},
+		{'name': 'numcols', 'type': 'short'},
+		{'name': 'linked', 'type': 'short'},
+		{'name': 'offset', 'type': 'long'},
+		{'name': 'len', 'type': 'long'},
+	],
+	'SffSprHeader': [
+		{'name': 'groupno', 'type': 'short'},
+		{'name': 'imageno', 'type': 'short'},
+		{'name': 'w', 'type': 'short'},
+		{'name': 'h', 'type': 'short'},
+		{'name': 'x', 'type': 'short'},
+		{'name': 'y', 'type': 'short'},
+		{'name': 'linked', 'type': 'short'},
+		{'name': 'fmt', 'type': 'char'},
+		{'name': 'colordepth', 'type': 'char'},
+		{'name': 'offset', 'type': 'long'},
+		{'name': 'len', 'type': 'long'},
+		{'name': 'palindex', 'type': 'short'},
+		{'name': 'flags', 'type': 'short'},
+	],
 }
 
-func fw_align(file_wrapper, size):
-	var rest = file_wrapper['position'] % size
+func check_struct_aligment(file, position, size):
+	var rest = position % size
 	var padding = 0
 	if rest > 0:
+		print('padding')
 		padding = (size - rest)
-		file_wrapper['file'].get_8()
-		file_wrapper['position'] = file_wrapper['position'] + padding
+		file.get_buffer(padding)
+		position = position + padding
+	return position
 
-func fw_parse_struct(file_wrapper, struct_name):
+func parse_struct(file, struct_name):
 	var spec = specs[struct_name]
 	var result = {}
 	var value = null
-	var file = file_wrapper['file']
+	var position = 0
 
 	for field in spec:
-		if field['type'] == 'u_char':
+		if field['type'] == 'char':
 			value = file.get_8()
-			file_wrapper['position'] = file_wrapper['position'] + 1
-		elif field['type'] == 'u_long':
-			fw_align(file_wrapper, 4)
+			position = position + 1
+		elif field['type'] == 'short':
+			position = check_struct_aligment(file, position, 2)
+			value = file.get_16()
+			position = position + 2
+		elif field['type'] == 'long':
+			position = check_struct_aligment(file, position, 4)
 			value = file.get_32()
-			file_wrapper['position'] = file_wrapper['position'] + 4
+			position = position + 4
 		elif field['type'] == 'string':
 			value = file.get_buffer(field['length']).get_string_from_ascii()
-			file_wrapper['position'] = file_wrapper['position'] + field['length']
+			position = position + field['length']
 		elif field['type'] == 'bytes':
 			value = file.get_buffer(field['length'])
-			file_wrapper['position'] = file_wrapper['position'] + field['length']
+			position = position + field['length']
 		else:
+			print("WARNING: Not recognized type")
 			value = null
 		result[field['name']] = value
-
 	return result
+
+func rle8_decode(buffer):
+	print("Unsupported format: rle8")
+	get_tree().quit()
+
+func rle5_decode(buffer):
+	print("Unsupported format: rle5")
+	get_tree().quit()
+
+func lz5_decode(buffer):
+	print("Unsupported format: rle5")
+	get_tree().quit()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var file = File.new()
 	file.open("res://chars/kfm/kfm.sff", file.READ)
+	var header = parse_struct(file, 'SffHeader')
+	var palettes = []
+	var sprites = []
+	var struct = null
+	var buffer = null
+	var offset = 0
+
+	if header['signature'] != 'ElecbyteSpr' or header['compatverhi'] != 2:
+		print('Invalid sff file. Only sffv2 are supported')
+		get_tree().quit()
+
+	file.seek(header['first_palnode_offset'])
+
+	for i in range(0, header['total_palettes']):
+		struct = parse_struct(file, 'SffPalHeader')
+		palettes.append(struct)
+
+	file.seek(header['first_sprnode_offset'])
 	
-	var file_wrapper = {'file': file, 'position': 0}
-	var header = fw_parse_struct(file_wrapper, 'SffHeader')
-	print(header)
+	for i in range(0, header['total_frames']):
+		struct = parse_struct(file, 'SffSprHeader')
+		sprites.append(struct)
+
+	for pal in palettes:
+		if pal['len'] == 0: # Linked
+			pal['pal'] = palettes[pal['linked']]['pal'].duplicate()
+		elif pal['len'] > 0:
+			pal['pal'] = []
+			file.seek(header['ldata_offset'] + pal['offset'])
+			for i in range(0, pal['numcols']):
+				pal['pal'].append(file.get_32())
+
+	for spr in sprites:
+		if spr['len'] == 0: # Linked
+			spr['image'] = sprites[spr['linked']]['image']
+		elif spr['len'] > 0:
+			spr['image'] = null
+			if spr['flags'] == 0:
+				offset = header['ldata_offset']
+			else:
+				offset = header['tdata_offset']
+			offset += spr['offset']
+			buffer = file.get_buffer(spr['len'])
+			if spr['fmt'] == 2:
+				rle8_decode(buffer)
+			elif spr['fmt'] == 3:
+				rle5_decode(buffer)
+			elif spr['fmt'] == 4:
+				# lz5_decode(buffer)
+				pass
 
 	file.close()
