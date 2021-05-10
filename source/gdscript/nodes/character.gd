91393094a42dcd0cfc675e210afcae76af8bfb5c
Jeferson Daniel <jeferson.daniel412@gmail.com>
2021-05-10 18:26:17 -0300

    feat(trigger): e

--- source/gdscript/nodes/character.gd
+++ source/gdscript/nodes/character.gd
@@ -370,6 +370,8 @@ func get_context_variable(key):
         return hit_time <= 0
     if key == "canrecover":
         return is_falling() and received_hit_def and received_hit_def.fall_recover
+    if key == "e":
+        return 2.718281828
     if key.begins_with("var."):
         return int_vars[int(key.substr(4, key.length() - 1))]
     if key in state_variables:
