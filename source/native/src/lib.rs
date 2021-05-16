9b566f95c8f384e59d0b1e14fb0d3316e2e7c4a5
Jeferson Daniel <jeferson.daniel412@gmail.com>
2021-05-16 02:55:00 -0300

    chore(rust): implement fnt

--- source/native/src/lib.rs
+++ source/native/src/lib.rs
@@ -2,12 +2,14 @@ use gdnative::prelude::*;
 mod expression;
 mod sff;
 mod snd;
+mod fnt;
 
 // Function that registers all exposed classes to Godot
 fn init(handle: InitHandle) {
     handle.add_class::<expression::mugen_expression::MugenExpression>();
     handle.add_class::<sff::sff_parser::SffParser>();
     handle.add_class::<snd::snd_parser::SndParser>();
+    handle.add_class::<fnt::fnt_parser::FntParser>();
 }
 
 // Macro that creates the entry-points of the dynamic library.
